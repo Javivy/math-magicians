8609d4ff9c86463402500d943bd4643bee7c7390
Javivy <84439052+Javivy@users.noreply.github.com>
2022-10-24 18:52:38 -0400

    Fix linters errors

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,6 @@
+/* eslint-disable no-unused-vars */
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import App from './App.js';
 
 test('renders learn react link', () => {
   render(<App />);
