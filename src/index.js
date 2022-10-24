3e6f4d6ba4f4d8566753ffa94df0a620c1a500ab
Javivy <84439052+Javivy@users.noreply.github.com>
2022-10-24 19:36:59 -0400

    Fix react errors

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
-import ReactDOM from 'react-dom/client.js';
+import ReactDOM from '../node_modules/react-dom/client.js';
 import './index.css';
 import App from './App.js';
 import reportWebVitals from './reportWebVitals.js';
