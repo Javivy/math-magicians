da6f0dbee4826cb960486400d5c7f274810b315a
Javivy <84439052+Javivy@users.noreply.github.com>
2022-10-25 20:51:54 -0400

    Use the component in App.js and compile in index.js

---
+++ src/App.js
@@ -0,0 +1,15 @@
+/* eslint-disable class-methods-use-this */
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import Calculator from './components/Calculator.js';
+import './index.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <Calculator />
+    );
+  }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 /* eslint-disable no-unused-vars */
 import ReactDOM from 'react-dom/client';
-import Calculator from './components/Calculator.js';
-import './index.css';
+import App from './App.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Calculator />);
+root.render(<App />);
